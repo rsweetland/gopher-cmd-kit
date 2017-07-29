56e91d97631bd6c7816ab19a7abb6406d81cee64
andylibrian <andy.librian@gmail.com>
2017-07-29 19:08:05 +0700

    validate fut format

--- handlers/onPreview.js
+++ handlers/onPreview.js
@@ -20,61 +20,103 @@ const config = require('../config');
 const futUtils = require('./../lib/futUtils');
 
 module.exports.main = (event, context, callback) => {
-	debug('onPreview: Webhook Received:', event);
+	// debug('onPreview: Webhook Received:', event);
 	let fut = new futUtils(event, context, callback);
 	if (!fut.webhookValidated)
 		return fut.respondError('Webhook validation failed');
 
-	var response = {
-		"valid": true,
-		"complete": true,
-		"email_address": "kitchen.sink@gopher.email",
-		"description": "This is going to do something",
-		"email_field": "bcc",
-		"autocomplete": {
-		"command": "kitchen",
-	    "params": [
-	      {
-	        "param_name": "object",   // after the user kitchen "kitchen", all these handy objects are shown
-	        "options": [
-	          {
-	            "option_name": "sink",
-	            "description": "Taco Corp > Tacos > 2016 Redesign > designs"
-	          },
-	          {
-	            "option_name": "soap",
-	            "description": "Taco Corp > Tacos > 2016 Redesign > proposals"
-	          },
-	          {
-	            "option_name": "faucet",
-	            "description": "Venter Surboards > 2017 > Layouts"
-	          },
-	          {
-	            "option_name": "dishes",
-	            "description": "Venter Surboards > 2017 > Layouts"
-	          }
-	        ]
-	      },
-	      {
-	        "param_name": "when",   // after the user types "kitchen", they are shown all of these
-	        "options": [
-	          {
-	            "option_name": "1day"
-	          },
-	          {
-	            "option_name": "1week",
-	          },
-	          {
-	            "option_name": "1month",
-	          }
-	        ]
-	      }
-	    ]
-	  }
-	};
+    let bodyJson = {};
+    try {
+        bodyJson = JSON.parse(event.body);
+    } catch (e) {
+    }
 
-	if (fut.isSimulation)
-		return fut.respondOk(response);
+    const command = _.get(bodyJson, 'command', '');
+    const commandSplit = command.split('.');
+    let commandParams = [];
+    let flag = commandSplit[0];
 
-	return fut.respondOk(response);
+    // has params
+    if (commandSplit.length > 1) {
+        commandParams = commandSplit.slice(1);
+    }
+
+    let valid = true;
+    let complete = false;
+
+    if (commandParams.length == 2) {
+        complete = true;
+    }
+
+    var response = {
+      "valid": valid,
+      "complete": complete,
+      "email_address": command + "@gopher.email",
+      "description": "This is going to do something",
+      "email_field": "bcc",
+      "autocomplete": {
+      "command": "ingatkan",
+        "params": [
+          {
+            "param_name": "who",   // after the user kitchen "kitchen", all these handy objects are shown
+            "options": [
+              {
+                "option_name": "me",
+                "description": "Remind me only"
+              },
+              {
+                "option_name": "everyone",
+                "description": "Everyone in this email"
+              }
+              ]
+          },
+          {
+            "param_name": "when",   // after the user types "kitchen", they are shown all of these
+            "options": [
+              {
+                "option_name": "1day"
+              },
+              {
+                "option_name": "1week",
+              },
+              {
+                "option_name": "1month",
+              }
+            ]
+          }
+        ]
+      }
+    };
+
+    if (!valid) {
+      // early return?
+    }
+
+    if (commandParams.length > 1) {
+      fut.getClient().resolveFormat({'format': commandParams[1]}, function (err, resolveFormatResponse) {
+        if (err) {
+          debug('status code', err.statusCode);
+          // invalid format
+          if (err.statusCode == 400) {
+            response.valid = false;
+            response.complete = false;
+          }
+        } else {
+          debug(resolveFormatResponse);
+          response.valid = response.valid && _.get(resolveFormatResponse, 'valid', false);
+        }
+
+        if (!response.valid) {
+          response.complete = false;
+        }
+
+        return fut.respondOk(response);
+      });
+    } else {
+      if (!response.valid) {
+        response.complete = false;
+      }
+
+      return fut.respondOk(response);
+    }
 }
